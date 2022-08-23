d9e6e50bb4e17212e4a3529b71d8d8be2b3359f8
Alana <halo2382001@yahoo.com>
2022-08-23 15:09:51 -0400

    working to set up testing in graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,13 @@
+// Resolvers are simply the functions we connect to each query or mutation type definition that perform the CRUD actions that each query or mutation is expected to perform.
+const { User, Book } = require('../models')
+
+
 const resolvers = {
-    Query: {
-        helloWorld: () => {
-            return "Hello World!"
-        }
-    }
-}
+  Query: {
+    books: () => {
+      return Book.find().sort({ title });
+    },
+  },
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,22 @@
+// typeDefs are literally defining every piece of data that the client can expect to work with through a query or mutation.
+// Think of this as not only defining the API endpoint, but also defining the exact data and parameters that are tied to that endpoint.
 // import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
 // create our typeDefs, in=between the backticks
 const typeDefs = gql`
-type Query {
-    helloWorld: String
-}
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    books: [Book]
+  }
 `;
 
 // export the typeDefs
