b3e27c8c03e034ad18b4232dfab3b27eda865ec8
Alana <halo2382001@yahoo.com>
2022-08-24 19:23:25 -0400

    saving files worked on with tutor

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     // <ApolloProvider client={client}>
+    // Switch must be changed to router below and up in the import
     <Router>
       <>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<SearchBooks/>} />
-          <Route exact path='/saved' element={<SavedBooks/>} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          {/* switched from components */}
+          <Route exact path="/" element={<SearchBooks />} />
+          <Route exact path="/saved" element={<SavedBooks />} />
+          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
         </Routes>
       </>
     </Router>
