60587659bb2f2a011acb5ce52975c76b2e6f4c66
Alana <halo2382001@yahoo.com>
2022-08-29 18:19:31 -0400

    finished book-mutations and set up one start point for the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    // <ApolloProvider client={client}>
-    // Switch must be changed to router below and up in the import
+    <ApolloProvider client={client}>
+    {/* // Switch must be changed to router below and up in the import */}
     <Router>
       <>
         <Navbar />
@@ -19,7 +34,7 @@ function App() {
         </Routes>
       </>
     </Router>
-    // </ApolloProvider>
+    </ApolloProvider>
   );
 }
 
