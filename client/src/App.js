9dedc54c89bb85ef5427304384d8ff564917fb8e
Alana <halo2382001@yahoo.com>
2022-08-30 14:10:47 -0400

    updated saved books

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,45 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    {/* // Switch must be changed to router below and up in the import */}
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          {/* switched from components */}
-          <Route exact path="/" element={<SearchBooks />} />
-          <Route exact path="/saved" element={<SavedBooks />} />
-          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-        </Routes>
-      </>
-    </Router>
+      {/* // Switch must be changed to router below and up in the import */}
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            {/* switched from components */}
+            <Route exact path="/" element={<SearchBooks />} />
+            <Route exact path="/saved" element={<SavedBooks />} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Routes>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Jumbotron,
   Container,
@@ -5,74 +6,57 @@ import {
   Card,
   Button,
 } from "react-bootstrap";
+
 import { useQuery, useMutation } from "@apollo/client";
-// import { SAVE_BOOK } from "../utils/mutations";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
-// import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
-const SavedBooks = (props) => {
+const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-
+  const [deleteBook] = useMutation(REMOVE_BOOK);
   const userData = data?.me || {};
 
-  // const [userData, setUserData] = useState({});
-
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error("something went wrong!");
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
+  if (!userData?.username) {
+    return (
+      <h4>
+        You need to be signed in to see this page. Please sign in!
+      </h4>
+    );
+  }
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
     if (!token) {
       return false;
     }
+
     try {
-      // const response = await deleteBook(bookId, token);
-      await removeBook({
-        variables: { bookId },
+      await deleteBook({
+        variables: { bookId: bookId },
+        update: (cache) => {
+          const data = cache.readQuery({ query: GET_ME });
+          const userDataCache = data.me;
+          const savedBooksCache = userDataCache.savedBooks;
+          const updatedBookCache = savedBooksCache.filter(
+            (book) => book.bookId !== bookId
+          );
+          data.me.savedBooks = updatedBookCache;
+          cache.writeQuery({
+            query: GET_ME,
+            data: { data: { ...data.me.savedBooks } },
+          });
+        },
       });
-      //   if (!response.ok) {
-      //     throw new Error("something went wrong!");
-      //   }
-      //   const updatedUser = await response.json();
-      //   setUserData(updatedUser);
-      //   // upon success, remove book's id from localStorage
+      // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
-
   // if data isn't here yet, say so
   if (loading) {
     return <h2>LOADING...</h2>;
@@ -107,12 +91,19 @@ const SavedBooks = (props) => {
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className="small">Authors: {book.authors}</p>
+                  {book.link ? (
+                    <Card.Text>
+                      <a href={book.link} target="_blank">
+                        More Information on Google Books
+                      </a>
+                    </Card.Text>
+                  ) : null}
                   <Card.Text>{book.description}</Card.Text>
                   <Button
                     className="btn-block btn-danger"
                     onClick={() => handleDeleteBook(book.bookId)}
                   >
-                    Delete this Book!
+                    Delete this Book
                   </Button>
                 </Card.Body>
               </Card>
